6bbb4bad51657dca3bcb71e149ceff10155d1b98
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-11-06 11:53:26 +0000

    Created check function in App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const location = search.value;
-  messageOne.textContent = "Loading...";
-  messageTwo.textContent = "";
-  fetch(`/weather?address=${location}`).then((response) => {
-    response.json().then(({ location, forecast, error }) => {
-      if (error) {
-        messageOne.textContent = error;
-      } else if (!location) {
-        messageOne.textContent = "Please enter a location.";
-        messageTwo.textContent = "";
-      } else {
-        messageOne.textContent = location;
-        messageTwo.textContent = forecast;
-      }
+if(weatherForm) {
+  weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const location = search.value;
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
+    fetch(`/weather?address=${location}`).then((response) => {
+      response.json().then(({ location, forecast, error }) => {
+        if (error) {
+          messageOne.textContent = error;
+        } else if (!location) {
+          messageOne.textContent = "Please enter a location.";
+          messageTwo.textContent = "";
+        } else {
+          messageOne.textContent = location;
+          messageTwo.textContent = forecast;
+        }
+      });
     });
   });
-});
+}
+
