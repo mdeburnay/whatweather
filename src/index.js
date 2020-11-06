aec912e950b54e8592946c6799e443fdfa5c1ebd
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-11-06 09:11:17 +0000

    Moved files to src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./frontend/App";
+import App from "./App";
 import * as serviceWorker from "./frontend/serviceWorker";
 
 ReactDOM.render(
