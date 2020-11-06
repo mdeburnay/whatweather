018ffa92111f6ddf5dbf4fc2840d491fe2b02b0f
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-11-06 12:44:17 +0000

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-weatherForm.onload = function () {
+window.onload = function () {
   weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = search.value;
